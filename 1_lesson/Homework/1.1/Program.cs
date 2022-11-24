67af8fcdd7a39ab36736bdda6f8f3fcf9c766992
Elena <shal226@rambler.ru>
2022-11-24 22:37:04 +0300

    Добавили первую задачу из ДЗ

---
+++ 1_lesson/Homework/1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("write number1");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("write number2");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("number 1 - большее, number 2 - меньшее");
+}
+
+else if (num2 > num1)
+{
+    Console.WriteLine("number 2 - большее, number 1 - меньшее");
+}
+
+else
+{
+    Console.WriteLine("числа равны");
+}
