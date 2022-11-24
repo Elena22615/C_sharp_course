9e15c23615b7435fe33d5fda7e494bfc55d47a26
Elena <shal226@rambler.ru>
2022-11-24 14:30:41 +0300

    Add second

---
+++ 1_lesson/1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Write number 1");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write number 2");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1*num1)
+{
+ 
+ Console.WriteLine("Yes"); 
+}
+
+else
+    Console.WriteLine("no");
+
