597e88da394d13e8fe1ddd1102da6a27beb58b9a
Elena <shal226@rambler.ru>
2022-11-25 10:21:42 +0300

    Добавили третью  задачу из ДЗ

---
+++ 1_lesson/Homework/1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine());
+if(num1%2==0)
+{
+    Console.WriteLine($"Число {num1} четное");
+}
+else
+{
+    Console.WriteLine($"Число {num1} нечетное");
+}
