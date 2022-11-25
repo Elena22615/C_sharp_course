55e57dc38db255977ee817d812baeb81d2c42222
Elena <shal226@rambler.ru>
2022-11-25 10:06:11 +0300

    Добавили иторую  задачу из ДЗ

---
+++ 1_lesson/Homework/1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if(a<b)
+{
+    a=b;
+}
+if(a<c)
+{
+    a=c;
+}
+Console.WriteLine($"max={a}");
