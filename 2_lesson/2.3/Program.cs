53e1f513ab9e5728c70095bdca59717855b0018c
Elena <shal226@rambler.ru>
2022-11-27 23:06:25 +0300

    Task 2.3

---
+++ 2_lesson/2.3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Write a number");
+int num = int.Parse(Console.ReadLine());
+
+string ABC(int num)
+{
+if (num % 7 == 0 && num % 23 == 0)
+    return "divisible";
+else
+    return "not divisible";
+
+}
+Console.WriteLine(ABC(num));
+
