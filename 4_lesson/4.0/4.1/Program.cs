0071cc0e005b725a1dcfc5f2f8c1a37c60cce4a8
Elena <shal226@rambler.ru>
2022-12-02 11:41:23 +0300

    task 4.1

---
+++ 4_lesson/4.0/4.1/Program.cs
@@ -0,0 +1,11 @@
+﻿int NumbCount(int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        n = n / 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine(NumbCount(int.Parse(Console.ReadLine())));
