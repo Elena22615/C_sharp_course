52c55f6313f98e753b727184bb06e4cad4facad1
Elena <shal226@rambler.ru>
2022-12-06 20:24:15 +0300

    task 4.3

---
+++ 4_lesson/4.0/4.2/Program.cs
@@ -0,0 +1,10 @@
+﻿int Proiz(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum = all_sum * i;
+    }
+    return all_sum;
+}
+Console.WriteLine(Proiz(int.Parse(Console.ReadLine())));
