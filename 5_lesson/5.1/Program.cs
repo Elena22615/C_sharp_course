abbe742a4dbdb2fad94f86db929401985a536f47
Elena <shal226@rambler.ru>
2022-12-09 12:00:29 +0300

    lesson 5

---
+++ 5_lesson/5.1/Program.cs
@@ -0,0 +1,33 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(-100, 100);
+    return arr;
+}
+
+void NumsChange(int[] arr_1)
+{
+       for (int i = 0; i < arr_1.Length; i++)
+        arr_1[i] = -arr_1[i];
+   
+}
+
+int[] arr_1 = MassNums(int.Parse(Console.ReadLine()));
+                   
+Print(arr_1);
+NumsChange(arr_1);
+Print(arr_1);
+
+
+// Замена положительных элементов отрицательными и обратно
---
+++ 5_lesson/5.3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Загадайте одномерный массив из 123 случайных чисел. Найти кол-во эл массива, значения которых лежат в отрезке [10,99]
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(-100, 1000);
+    return arr;
+}
+
+int CountDiap(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count++;
+        }
+            
+    }
+    return count;
+}
+
+int[] arr_1 = MassNums(int.Parse(Console.ReadLine()));                       
+Print(arr_1);
+Console.Write(CountDiap(arr_1));
