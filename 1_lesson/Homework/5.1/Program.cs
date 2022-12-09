4324b203f41d1c0e0c2b323eaeb4a61438a9b460
Elena <shal226@rambler.ru>
2022-12-09 12:57:32 +0300

    homework 5

---
+++ 1_lesson/Homework/5.1/Program.cs
@@ -0,0 +1,36 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(100, 1000);
+    return arr;
+}
+
+int EvenNums(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+
+int[] arr_1 = MassNums(int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.Write(EvenNums(arr_1));
---
+++ 1_lesson/Homework/5.3/Program.cs
@@ -0,0 +1,43 @@
+﻿void Print(double[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+
+}
+
+double[] MassNums(int size, int from, int to)
+{
+    double[] arr = new double[size];
+    Random n_new = new Random();
+
+    for (int i = 0; i < size; i++)
+        arr[i] = Math.Round(n_new.NextDouble() * (from + to) - from, 2);
+    return arr;
+}
+
+void MinMax(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+
+        if (max<arr[i])
+            max=arr[i];
+        else if(min>arr[i])
+            min=arr[i];    
+      
+    }
+    Console.Write($"Max: {max}, min:  {min}  ");
+    Console.WriteLine($"Max-Min: {max} - ({min}) = {Math.Round(max-min,2)}");
+}
+
+double[] arr_1 = MassNums(int.Parse(Console.ReadLine()),
+                          int.Parse(Console.ReadLine()),
+                          int.Parse(Console.ReadLine()));
+
+Print(arr_1);
+MinMax(arr_1);
