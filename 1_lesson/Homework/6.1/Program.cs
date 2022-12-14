33e645e22d4617ee401a9cb640c3e4763d36240b
Elena <shal226@rambler.ru>
2022-12-14 14:48:01 +0300

    homework 6

---
+++ 1_lesson/Homework/6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Num(int n)
+{
+    int size = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{i} ");
+        int a = int.Parse(Console.ReadLine());
+        if (a > 0) size = size + 1;
+    }
+    return size;
+}
+
+Console.Write("Ввод чисел  ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Чисел больше нуля: {Num(a)}");
---
+++ 1_lesson/Homework/6.2/Program.cs
@@ -0,0 +1,15 @@
+﻿void Equation(double k1, double k2, double b1, double b2)
+{
+    double a = k1 - k2;
+    if (a != 0)
+    {
+        double x = (b2 - b1) / (a);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка: ({x}; {y}");
+    }
+}
+
+Equation(int.Parse(Console.ReadLine()),
+         int.Parse(Console.ReadLine()),
+         int.Parse(Console.ReadLine()),
+         int.Parse(Console.ReadLine()));
