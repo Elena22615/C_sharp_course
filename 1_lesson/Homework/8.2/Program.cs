38dff3ae5a9cb59c74544c04c9505478cfc7952b
Elena <shal226@rambler.ru>
2022-12-21 14:33:20 +0300

    homework 8

---
+++ 1_lesson/Homework/8.2/Program.cs
@@ -0,0 +1,60 @@
+﻿void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void Mass(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    int amount = 1000;
+    int num;
+    int smalest = 0;
+
+    for (int i = 0; i < row_size; i++)
+    {
+        num = 0;
+        for (int j = 0; j < column_size; j++)
+            num += arr[i, j];
+        Console.Write($"{num,4} ");
+
+        if (amount > num)
+        {
+            amount = num;
+            smalest = i;
+        }
+    }
+    Console.WriteLine();
+    Console.Write($"{smalest + 1}");
+}    
+
+
+    Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+Mass(arr_1);
---
+++ 1_lesson/Homework/8.3/Program.cs
@@ -0,0 +1,51 @@
+﻿void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+int[,] Mass(int[,] arr_first, int[,] arr_second)
+{
+    int row_size = arr_first.GetLength(0);
+    int column_size = arr_first.GetLength(1);
+    int[,] pr_matrix = new int[row_size, column_size];
+
+    if (row_size != arr_second.GetLength(0) || column_size != arr_second.GetLength(1)) return pr_matrix;
+
+    for (int i = 0; i < row_size; i++)
+        for (int j = 0; j < column_size; j++)
+            pr_matrix[i, j] = arr_first[i, j] * arr_second[i, j];
+    return pr_matrix;
+
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+Mass(arr_1);
+Print(arr_1);
